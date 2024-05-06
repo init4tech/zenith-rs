6bac80be44e49e3c4d7c7e0f166ec78687b6c40c
James <james@prestwi.ch>
2024-05-06 13:12:53 -0400

    feat: fromimpl

--- crates/types/src/bindings.rs
+++ crates/types/src/bindings.rs
@@ -5,3 +5,15 @@ sol!(
     Zenith,
     "abi/zenith.json"
 );
+
+impl From<&Zenith::BlockSubmitted> for Zenith::BlockHeader {
+    fn from(event: &Zenith::BlockSubmitted) -> Zenith::BlockHeader {
+        Zenith::BlockHeader {
+            rollupChainId: event.rollupChainId,
+            sequence: event.sequence,
+            confirmBy: event.confirmBy,
+            gasLimit: event.gasLimit,
+            rewardAddress: event.rewardAddress,
+        }
+    }
+}
