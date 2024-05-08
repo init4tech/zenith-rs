1881b2d5bac6c430a74fa88e9116a6eb8479c615
James <james@prestwi.ch>
2024-05-08 12:26:34 -0400

    feat: getters for blockheader

--- crates/types/src/bindings.rs
+++ crates/types/src/bindings.rs
@@ -1,3 +1,4 @@
+use alloy_primitives::Address;
 use alloy_sol_types::sol;
 
 sol!(
@@ -16,5 +17,32 @@ impl From<&Zenith::BlockSubmitted> for Zenith::BlockHeader {
             gasLimit: event.gasLimit,
             rewardAddress: event.rewardAddress,
         }
+    }
+}
+
+impl Zenith::BlockHeader {
+    /// Get the chain ID of the block (discarding high bytes).
+    pub fn chain_id(&self) -> u64 {
+        self.gasLimit.as_limbs()[0]
+    }
+
+    /// Get the sequence of the block (discarding high bytes).
+    pub fn sequence(&self) -> u64 {
+        self.sequence.as_limbs()[0]
+    }
+
+    /// Get the confirm by time of the block (discarding high bytes).
+    pub fn confirm_by(&self) -> u64 {
+        self.gasLimit.as_limbs()[0]
+    }
+
+    /// Get the gas limit of the block (discarding high bytes).
+    pub fn gas_limit(&self) -> u64 {
+        self.gasLimit.as_limbs()[0]
+    }
+
+    /// Get the reward address of the block.
+    pub fn reward_address(&self) -> Address {
+        self.rewardAddress
     }
 }
