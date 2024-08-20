7934e2d6804e32dd1ff5523c54bc2e39179e7fb1
evalir <e@evalir.xyz>
2024-08-20 14:14:13 +0200

    feat: expose HostOrders

--- crates/types/src/lib.rs
+++ crates/types/src/lib.rs
@@ -12,7 +12,7 @@
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
 
 mod bindings;
-pub use bindings::{Passage, RollupOrders, RollupPassage, Transactor, Zenith};
+pub use bindings::{HostOrders, Passage, RollupOrders, RollupPassage, Transactor, Zenith};
 
 mod block;
 pub use block::{decode_txns, encode_txns, Alloy2718Coder, Coder, ZenithBlock, ZenithTransaction};
