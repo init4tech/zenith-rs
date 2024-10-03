6adb5e054497c2a6cef2195b9fd7b1cf1c5f2779
Anna Carroll <anna.s.carroll@gmail.com>
2024-10-03 17:01:47 -0500

    Fix: txn cache integration test

--- crates/builder/tests/tx_poller_test.rs
+++ crates/builder/tests/tx_poller_test.rs
@@ -7,29 +7,39 @@ mod tests {
 
     use builder::config::BuilderConfig;
     use builder::tasks::{block::BlockBuilder, tx_poller};
+    use reqwest::Url;
 
     #[ignore = "integration test"]
     #[tokio::test]
     async fn test_tx_roundtrip() {
         // create a new test environment
-        let client = reqwest::Client::new();
         let (_, config) = setup_test_builder().await;
+
+        // post a tx to the cache
+        post_tx(config.clone()).await;
+
+        // assert that we parsed at least one transaction
+        let got = tx_poller::TxPoller::new(&config).check_tx_pool().await.unwrap();
+        assert!(!got.is_empty());
+    }
+
+    async fn post_tx(config: BuilderConfig) {
+        // create a new test environment
+        let client = reqwest::Client::new();
 
         // create a new signed test transaction
         let wallet = PrivateKeySigner::random();
         let tx_envelope = new_test_tx(&wallet);
 
+        let url: Url = Url::parse(&config.tx_pool_url)?.join("add")?;
+
         // send that transaction to ensure there is at least one tx in pool to parse
         let _ = client
-            .post(config.tx_pool_url.to_string() + "/add")
+            .post(url)
             .json(&tx_envelope)
             .send()
             .await
             .unwrap();
-
-        // assert that we parsed at least one transaction
-        let got = tx_poller::TxPoller::new(&config).check_tx_pool().await.unwrap();
-        assert!(!got.is_empty());
     }
 
     // returns a new signed test transaction with blank values
